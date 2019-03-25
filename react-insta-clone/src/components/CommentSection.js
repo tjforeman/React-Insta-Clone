689e017baf134fe9e1324179b086f8511b516d11
tjforeman <tylerforeman92@gmail.com>
2019-03-25 15:20:12 -0400

    components setup

---
+++ react-insta-clone/src/components/CommentSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CommentSection = props =>{
+    return(
+        <div className='comment-container'>
+        
+        </div>
+    )
+}
+export default CommentSection
