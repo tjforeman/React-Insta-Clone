31b72f7ca2b1ce12e2bf75703a36328e3efec57d
tjforeman <tylerforeman92@gmail.com>
2019-03-27 15:17:52 -0400

    postpage created and working

--- react-insta-clone/src/App.js
+++ react-insta-clone/src/App.js
@@ -1,41 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar'
-import PostContainer from './components/PostContainer'
-import dummyData from './dummy-data';
-import './components/components.css'
+import PostsPage from './components/PostsPage'
 
 
 class App extends Component {
   constructor(){
     super();
-    this.state={
-      users :[],
-      filteredPosts:[],
-    }
   }
-  componentDidMount(){
-    this.setState({users: dummyData});
-  }
-  searchItems = event =>{
-    let search= this.state.users.filter(post =>{
-      if (post.username.includes(event.target.value)) {
-        console.log(event.target.value)
-        return post;
-      }
-    });
-    this.setState({filteredPosts:search});
-  };
-
 
   render() {
     return (
       <div className="App">
-        <SearchBar searchItems={this.searchItems}  />
-        <PostContainer users={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts: this.state.users
-          } />
+        <PostsPage />
       </div>
     );
   }
