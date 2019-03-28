d42ed3909a81ebc6de6ebbd1baa7d243229441f8
tjforeman <tylerforeman92@gmail.com>
2019-03-28 15:44:01 -0400

    searchbar refactored

--- react-insta-clone/src/components/SearchBar.js
+++ react-insta-clone/src/components/SearchBar.js
@@ -4,23 +4,51 @@ import insta from './insta-insta.jpg';
 import compass from './insta-compass.png';
 import heart from './insta-heart.png'
 import profile from './insta-profile.png'
+import styled from "styled-components";
 
+const LogoHeader = styled.div`
+display:flex;
+align-items:center;
+width:750px;
+margin-left:25%;
+justify-content:space-between;
+margin-bottom:30px;
+`;
+const Box = styled.div `
+display:flex;
+align-content:center;
+`;
 
+const LogoImage= styled.img`
+width:120px;
+height:50px;
+`;
+
+const IconImage=styled.img`
+width:50px;
+height:40px;
+`;
+
+const Inputbar= styled.input`
+height:20px;
+text-align:center;
+border-radius:8px;
+`;
 
 const SearchBar = props =>{
     return (
-        <div className='searchbar-container'>
-        <div ClassName='head'>
-        <img className='camera' src={camera}/>
-         <img className='insta' src={insta}/>
-         </div>
-         <input className='input' type='text' placeholder='search'  onKeyDown={props.searchItems}></input>
-         <div className='icons'>
-         <img className='compass' src={compass} />
-         <img className='heart' src={heart} />
-         <img className ='profile'src={profile} />
-         </div>
-        </div>
+        <LogoHeader>
+        <Box>
+        <IconImage src={camera}/>
+         <LogoImage src={insta}/>
+         </Box>
+         <Inputbar type='text' placeholder='search' onKeyDown={props.searchItems}></Inputbar>
+         <Box>
+         <IconImage src={compass} />
+         <IconImage src={heart} />
+         <IconImage src={profile} />
+         </Box>
+        </LogoHeader>
     )
 }
 export default SearchBar
